d93cceadfbb2e248dd99974b30e0206fb1c0361c
jnog93 <joao.nogueira@raceland.pt>
2021-11-25 17:00:07 +0000

    alteraçoes finais

--- src/util/documentation-url.ts
+++ src/util/documentation-url.ts
@@ -1,7 +1,13 @@
 import { HomeAssistant } from "../types";
 
 export const documentationUrl = (hass: HomeAssistant, path: string) =>
-  `https://automacaoraceland.pt/docs-domotica/`;
+  `https://automacaoraceland.pt/docs-domotica/#${path}#${
+      hass.config.version.includes("b")
+        ? "rc"
+        : hass.config.version.includes("dev")
+        ? "next"
+        : "www"
+    }`;
   // `https://${
   //   hass.config.version.includes("b")
   //     ? "rc"
