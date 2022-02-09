490c745e1e07b226a1a48ec34d5c27784dc5e232
jnog93 <joao.nogueira@raceland.pt>
2022-02-09 15:21:17 +0000

    correçao de erros e fix preview

--- src/panels/lovelace/editor/card-editor/hui-card-picker.ts
+++ src/panels/lovelace/editor/card-editor/hui-card-picker.ts
@@ -341,7 +341,7 @@ export class HuiCardPicker extends LitElement {
 
         }
         .preview > :first-child {
-          zoom: 0.9;
+          zoom: 0.6;
           display: block;
           width: 100%;
         }
